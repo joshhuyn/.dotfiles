4714fdf817fd0e836d46747a5e9b5b18a7dc6fcc
Josh035 <jh_crafter@yahoo.com>
2023-02-19 15:09:29 +0100

    added config for top panel

---
+++ kde-panels/README.md
@@ -0,0 +1,16 @@
+# commands
+
+## install
+```
+kpackagetool5 --type=Plasma/LayoutTemplate -i /path/to/package
+```
+
+## list entries
+```
+kpackagetool5 --list --type Plasma/Applet --global
+```
+
+## interactive console
+```
+plasma-interactiveconsole
+```
---
+++ kde-panels/panel-top/contents/layout.js
@@ -0,0 +1,10 @@
+var panel = new Panel();
+
+panel.location = "top";
+panel.height = gridUnit * 1.5;
+
+panel.addWidget("kicker");
+panel.addWidget("panelspacer");
+panel.addWidget("digitalclock");
+panel.addWidget("panelspacer");
+panel.addWidget("org.kde.plasma.systemtray");
