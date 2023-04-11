472871e69da0ae411060f4858ea25c73ac852cc8
Josh035 <josh-it@outlook.com>
2023-04-11 13:16:22 +0200

    Panel: changed Kicker to kickerdash

--- kde-panels/panel-top/contents/layout.js
+++ kde-panels/panel-top/contents/layout.js
@@ -3,7 +3,7 @@ var panel = new Panel();
 panel.location = "top";
 panel.height = gridUnit * 1.5;
 
-panel.addWidget("org.kde.plasma.kicker");
+panel.addWidget("org.kde.plasma.kickerdash");
 panel.addWidget("org.kde.plasma.appmenu");
 panel.addWidget("org.kde.plasma.panelspacer");
 panel.addWidget("org.kde.plasma.digitalclock");
