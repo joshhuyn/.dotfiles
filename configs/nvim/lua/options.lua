267a86903ccb75260fb9882be1483c0a13deb106
JOH <joshua.huynh@ivfp.de>
2026-04-27 14:04:07 +0200

    nvim options

--- configs/nvim/lua/options.lua
+++ configs/nvim/lua/options.lua
@@ -2,5 +2,10 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
+o.relativenumber = true
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+o.tabstop = 4
+o.shiftwidth = 4
+o.wrap = false
